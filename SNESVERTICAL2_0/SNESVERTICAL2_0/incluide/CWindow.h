7e2a6957be2380c33d325ce0df9d408de551ed7d
serbolsd <32022302+serbolsd@users.noreply.github.com>
2019-09-18 14:29:15 -0500

    Clase de window con un enum para elegir el tipo de ventana (solo usaremos render window para poder pintar en ella los shapes/sprites), solo se puede abrir una ventana por objeto window, tiene las siguientes funciones: initWindow() : es para iniciar una ventana, se le dan de parametros el alto, el ancho, el nombre y el tipo de ventana (por default crea una render window). getWindow() : retorna el puntero de la ventana. closeWindow() : se llama cuando se selecciona la opcion de cerrar la ventana, esta funcion libera todo lo usado en memoria que se uso para la ventana. *window : es un puntero a la venta, este es privado

---
+++ SNESVERTICAL2_0/SNESVERTICAL2_0/incluide/CWindow.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <string>
+#include <iostream>
+enum WINDOWTYPE
+{
+	TRenderWindow = 0,
+	THandleWindow,
+	Twindow
+};
+class CWindow
+{
+	
+public:
+	CWindow();
+	~CWindow();
+	void initWindow(const int & width = 600, const int &heihgt = 500,const std::string & windowName = "Uname", const WINDOWTYPE &tipe =WINDOWTYPE::TRenderWindow);
+	void onDelete();
+	bool onUpdate();
+	bool onUpdateRenderW();
+	bool onUpdateHandleW();
+	bool onUpdateW();
+	void closeWindow();
+	sf::RenderWindow* getWindow();
+	void setEvent(sf::Event *eve) { event = eve; };
+private:
+	sf::RenderWindow* renderWindow = nullptr;
+	sf::WindowHandle* handleWindow = nullptr;
+	sf::Window* window = nullptr;
+	bool isWindowInit = false;
+	WINDOWTYPE *tipeWin = nullptr;
+	sf::Event* event=nullptr;
+
+
+};
+
---
+++ SNESVERTICAL2_0/SNESVERTICAL2_0/source/CWindow.cpp
@@ -0,0 +1,187 @@
+#include "../incluide/CWindow.h"
+
+CWindow::CWindow()
+{
+	renderWindow = nullptr;
+}
+
+CWindow::~CWindow()
+{
+}
+
+void CWindow::initWindow(const int & width, const int & heihgt, const std::string & windowName, const WINDOWTYPE & tipe)
+{
+	if (isWindowInit)
+	{
+		std::cout << "this Windows already init\n";
+	}
+	tipeWin = new WINDOWTYPE;
+	std::string tipoWindow;
+	switch (tipe)
+	{
+	case WINDOWTYPE::TRenderWindow:
+		renderWindow = new sf::RenderWindow(sf::VideoMode(width, heihgt), windowName);
+		tipoWindow = "RenderWindow";
+		break;
+	case WINDOWTYPE::THandleWindow:
+		handleWindow = new sf::WindowHandle();
+		tipoWindow = "HandleWindow";
+		break;
+	case WINDOWTYPE::Twindow:
+		window = new sf::Window(sf::VideoMode(width, heihgt), windowName);
+		tipoWindow = "window";
+		break;
+	default:
+		std::cout << "Invalid tipe\n";
+		return;
+		break;
+	}
+	*tipeWin = tipe;
+	std::cout << "Window " << windowName <<" tipe of "<<tipoWindow<< " SUCCES init\n";
+	isWindowInit = true;
+}
+
+void CWindow::onDelete()
+{
+	if (window!=nullptr)
+	{
+		delete window;
+		window=nullptr;
+	}
+	if (handleWindow!= nullptr)
+	{
+		delete handleWindow;
+		handleWindow = nullptr;
+	}
+	if (renderWindow!= renderWindow)
+	{
+		delete renderWindow;
+		renderWindow = nullptr;
+	}
+	if (event!=nullptr)
+	{
+		delete event;
+		event = nullptr;
+	}
+	if (tipeWin != nullptr)
+	{
+		delete tipeWin;
+		tipeWin = nullptr;
+	}
+}
+
+bool CWindow::onUpdate()
+{
+	switch (*tipeWin)
+	{
+	case WINDOWTYPE::TRenderWindow:
+		return renderWindow;
+		break;
+	case WINDOWTYPE::THandleWindow:
+		return handleWindow;
+		break;
+	case WINDOWTYPE::Twindow:
+		return window;
+		break;
+	default:
+		return false;
+		break;
+	}
+	return false;
+}
+
+bool CWindow::onUpdateRenderW()
+{
+	if (renderWindow->isOpen())
+	{
+
+		while (renderWindow->pollEvent(*event))
+		{
+			// "close requested" event: we close the window
+			if (event->type == sf::Event::Closed)
+			{ 
+				renderWindow->close();
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+bool CWindow::onUpdateHandleW()
+{
+	if (renderWindow->isOpen())
+	{
+
+		while (renderWindow->pollEvent(*event))
+		{
+			// "close requested" event: we close the window
+			if (event->type == sf::Event::Closed)
+			{
+				renderWindow->close();
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+bool CWindow::onUpdateW()
+{
+	if (renderWindow->isOpen())
+	{
+
+		while (renderWindow->pollEvent(*event))
+		{
+			// "close requested" event: we close the window
+			if (event->type == sf::Event::Closed)
+			{
+				renderWindow->close();
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+void CWindow::closeWindow()
+{
+	switch (*tipeWin)
+	{
+	case WINDOWTYPE::TRenderWindow:
+		renderWindow->close();
+		break;
+	case WINDOWTYPE::THandleWindow:
+		//handleWindow;
+		break;
+	case WINDOWTYPE::Twindow:
+		window->close();
+		break;
+	default:
+		nullptr;
+		break;
+	}
+}
+
+sf::RenderWindow * CWindow::getWindow()
+{
+	switch (*tipeWin)
+	{
+	case WINDOWTYPE::TRenderWindow:
+		return renderWindow;
+		break;
+	case WINDOWTYPE::THandleWindow:
+		//return handleWindow;
+		break;
+	case WINDOWTYPE::Twindow:
+		//return window;
+		break;
+	default:
+		return nullptr;
+		break;
+	}
+	return nullptr;
+}
