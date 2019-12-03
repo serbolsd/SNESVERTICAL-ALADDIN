c62cdb8f56db17c8a0b0660bf1abf8da4f7d5d88
serbolsd <32022302+serbolsd@users.noreply.github.com>
2019-12-02 21:00:00 -0600

    se creo el objeto enemy del cual van a derivar los enemigos

--- SNESVERTICAL2_0/SNESVERTICAL2_0/include/Enemy.h
+++ SNESVERTICAL2_0/SNESVERTICAL2_0/include/Enemy.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "GameObject.h"
+
+class Enemy : public GameObject
+{
+public:
+	Enemy() {};
+	~Enemy() {};
+	void onInit() {};
+	void onUpdate() {};
+	void draw(sf::RenderWindow& wind) {};
+	void onDelete() {};
+private:
+	bool seeAladdin=false;//if aladdin enter to the vision, this is true
+
+};
